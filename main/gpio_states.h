123f16049c844e6fda6948b48d94c0e884ec2080
Cxmrykk <140925712+camerikk@users.noreply.github.com>
2023-10-23 10:37:59 +1100

    Reverse GPIO order

--- main/gpio_states.h
+++ main/gpio_states.h
@@ -4,11 +4,11 @@
     #include <stdint.h>
 
     // extension board IO
-    #define GPIO_LAMP GPIO_NUM_46
-    #define GPIO_ACC GPIO_NUM_45
+    #define GPIO_LAMP GPIO_NUM_42
+    #define GPIO_ACC GPIO_NUM_43
     #define GPIO_RESET GPIO_NUM_44
-    #define GPIO_HOUR GPIO_NUM_43
-    #define GPIO_MINUTE GPIO_NUM_42
+    #define GPIO_HOUR GPIO_NUM_45
+    #define GPIO_MINUTE GPIO_NUM_46
 
     // maximum loop cycles to register GPIO event (prevents noise)
     #define GPIO_HOLD_COUNT 2
