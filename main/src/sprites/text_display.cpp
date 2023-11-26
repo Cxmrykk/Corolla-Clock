10975b15f1f1f29779603aa0432723bcd9a452b0
Cxmrykk <140925712+camerikk@users.noreply.github.com>
2023-11-26 13:34:23 +1100

    fix offset calculations

--- main/src/sprites/text_display.cpp
+++ main/src/sprites/text_display.cpp
@@ -3,6 +3,6 @@
 
 void draw_text_display(TFT_eSprite *sprite, int lines, int x, int y, const char **contents) {
     for (int i = 0; i < lines; i++) {
-        sprite->drawString((char*) contents[i], x, y + ((i - 1) * TEXT_HEIGHT_OFFSET), 4);
+        sprite->drawString((char*) contents[i], x, y + i * TEXT_HEIGHT_OFFSET, 4);
     }
 }
